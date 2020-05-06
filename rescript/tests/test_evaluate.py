78ae4e537bfbfac40a6bc70f90efe8303528ad82
nbokulich <nbokulich@gmail.com>
2020-05-06 13:34:36 -0700

    fix copyright headers

--- rescript/tests/test_evaluate.py
+++ rescript/tests/test_evaluate.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------------------------
-# Copyright (c) 2019--, QIIME 2 development team.
+# Copyright (c) 2020, QIIME 2 development team.
 #
-# Distributed under the terms of the Lesser GPL 3.0 licence.
+# Distributed under the terms of the Modified BSD License.
 #
 # The full license is in the file LICENSE, distributed with this software.
 # ----------------------------------------------------------------------------
