2f0cb9ee232bf37dedfbf90ffc8218babbbbd00f
Nicholas Bokulich <nbokulich@gmail.com>
2020-06-05 08:58:46 -0700

    new orient seqs method (#25)
    LGTM! :-)

---
+++ rescript/orient.py
@@ -0,0 +1,55 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+
+import tempfile
+from q2_types.feature_data import DNAFASTAFormat, DNAIterator
+
+from ._utilities import run_command
+
+
+def orient_seqs(sequences: DNAFASTAFormat,
+                reference_sequences: DNAFASTAFormat,
+                perc_identity: float = 0.9,
+                query_cov: float = 0.9,
+                threads: int = 1,
+                left_justify: bool = False,
+                ) -> (DNAFASTAFormat, DNAFASTAFormat):
+    matched_temp, notmatched = DNAFASTAFormat(), DNAFASTAFormat()
+    # use vsearch to search query seqs against reference database
+    # report orientation of query seqs relative to reference seqs.
+    with tempfile.NamedTemporaryFile() as out:
+        # note: qmask is disabled as DNAFASTAFormat requires all output seqs
+        # to be uppercase. Could loop through output seqs to convert to upper
+        # but which is faster: disabling masking or looping through with skbio?
+        cmd = ['vsearch', '--usearch_global', str(sequences),
+               '--matched', str(matched_temp), '--notmatched', str(notmatched),
+               '--db', str(reference_sequences), '--id', str(perc_identity),
+               '--maxaccepts', '1', '--strand', 'both', '--qmask', 'none',
+               '--query_cov', str(query_cov), '--threads', str(threads),
+               '--userfields', 'qstrand', '--userout', out.name]
+        if left_justify:
+            cmd.append('--leftjust')
+        run_command(cmd)
+        with open(out.name, 'r') as orient:
+            orientations = [line.strip() for line in orient]
+
+    # if any query seqs are in reverse orientation, reverse complement
+    if '-' in orientations:
+        matched = DNAFASTAFormat()
+        with matched.open() as out_fasta:
+            for seq, orientation in zip(
+                    matched_temp.view(DNAIterator), orientations):
+                if orientation == '+':
+                    seq.write(out_fasta)
+                elif orientation == '-':
+                    seq.reverse_complement().write(out_fasta)
+    else:
+        matched = matched_temp
+
+    return matched, notmatched
---
+++ rescript/tests/test_orient.py
@@ -0,0 +1,94 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+
+import qiime2
+from qiime2.plugin.testing import TestPluginBase
+from q2_types.feature_data import DNAIterator
+from qiime2.plugins import rescript
+
+
+import_data = qiime2.Artifact.import_data
+
+
+class TestOrientSeqs(TestPluginBase):
+    package = 'rescript.tests'
+
+    def setUp(self):
+        super().setUp()
+        self.seqs = import_data('FeatureData[Sequence]',
+                                self.get_data_path('mixed-orientations.fasta'))
+        self.ref = import_data(
+            'FeatureData[Sequence]', self.get_data_path('derep-test.fasta'))
+
+    def test_reorient_default(self):
+        # this test checks that expected IDs AND reoriented seqs are returned
+        reoriented, unmatched, = rescript.actions.orient_seqs(
+            sequences=self.seqs, reference_sequences=self.ref)
+        # all input seqs should pass, minus the two junk seqs. Those that pass
+        # are copies of the ref seqs with some mismatches/gaps inserted
+        # so the oriented seq ids should match the ref ids.
+        reoriented_seqs = {seq.metadata['id']: str(seq)
+                           for seq in reoriented.view(DNAIterator)}
+        exp_reoriented_seqs = {seq.metadata['id']: str(seq)
+                               for seq in self.ref.view(DNAIterator)}
+        self.assertEqual(reoriented_seqs.keys(), exp_reoriented_seqs.keys())
+        # the oriented seqs should also match the ref seqs, except for those
+        # with some mismatches inserted...
+        exp_reoriented_seqs['A1'] = (
+            'AAAAAAGCAGCCGCGGTAATACGGAGGGTGCGAGCGTTGTCCGGAATCACTGGGCGTAAAGGGCG'
+            'CGTAGGTGGCCCGTTAAGTGGCTGGTGAAATCCCGGGGCTCAACTCCGGGGCTGCCGGTCAGACT'
+            'GGCGAGCTAGAGCACGGTAGGGGCAGATGGAATTCCCGGTGTAGCGGTGGAATGCGTAGATATCG'
+            'GGAAGAATACCAGTGGCGAAGGCGTTCTGCTGGGCCGTTGCTGACACTGAGGCGCGACAGCGTGG'
+            'GGAGCAAACAGGATTAGATACCCTGGTAGTC')
+        exp_reoriented_seqs['A2'] = (
+            'AGCAGCCGCGGTAATACGGAGGGTGCGAGCGTTGTCCGGAATCACTGGGCGTAAAGGGCGCGTAG'
+            'GTGGCCCGTTAAGTGGCTGGTGAAATCCCGGGGCTCAACTCCGGGGCTGCCGGTCAGACTGGCGA'
+            'GGAAGAGCACGGTAGGGGCAGATGGAATTCCCGGTGTAGCGGTGGAATGCGTAGATATCGGGAAG'
+            'AATACCAGTGGCGAAGGCGTTCTGCTGGGCCGTTGCTGACACTGAGGCGCGACAGCGTGGGGAGC'
+            'AAACAGGATTAGATACCCTGGTAGTC')
+        exp_reoriented_seqs['A3'] = exp_reoriented_seqs['A3'][:-21] + 'T' * 21
+        self.assertEqual(
+            set(reoriented_seqs.values()), set(exp_reoriented_seqs.values()))
+        # make sure the junk sequences don't match anything and are discarded
+        unmatched_ids = {
+            seq.metadata['id'] for seq in unmatched.view(DNAIterator)}
+        exp_unmatched_ids = {'JUNK', 'MOREJUNK'}
+        self.assertEqual(unmatched_ids, exp_unmatched_ids)
+
+    def test_reorient_exact(self):
+        # just check correct IDs are returned; default test checks orientations
+        reoriented, unmatched, = rescript.actions.orient_seqs(
+            sequences=self.seqs, reference_sequences=self.ref,
+            perc_identity=1.0)
+        unmatched_ids = {
+            seq.metadata['id'] for seq in unmatched.view(DNAIterator)}
+        # seq A1 A2 and A3 were modified in self.seqs to have sundry mismatches
+        exp_unmatched_ids = {'JUNK', 'MOREJUNK', 'A1', 'A2', 'A3'}
+        self.assertEqual(unmatched_ids, exp_unmatched_ids)
+        reoriented_ids = {
+            seq.metadata['id'] for seq in reoriented.view(DNAIterator)}
+        exp_reoriented_ids = {seq.metadata['id'] for seq in
+                              self.ref.view(DNAIterator)} - exp_unmatched_ids
+        self.assertEqual(reoriented_ids, exp_reoriented_ids)
+
+    def test_reorient_left_justify(self):
+        # just check correct IDs are returned; default test checks orientations
+        reoriented, unmatched, = rescript.actions.orient_seqs(
+            sequences=self.seqs, reference_sequences=self.ref,
+            left_justify=True)
+        unmatched_ids = {
+            seq.metadata['id'] for seq in unmatched.view(DNAIterator)}
+        # seq A2 was modified in self.seqs to have gaps at the 5' end
+        exp_unmatched_ids = {'JUNK', 'MOREJUNK', 'A2'}
+        self.assertEqual(unmatched_ids, exp_unmatched_ids)
+        reoriented_ids = {
+            seq.metadata['id'] for seq in reoriented.view(DNAIterator)}
+        exp_reoriented_ids = {seq.metadata['id'] for seq in
+                              self.ref.view(DNAIterator)} - exp_unmatched_ids
+        self.assertEqual(reoriented_ids, exp_reoriented_ids)
