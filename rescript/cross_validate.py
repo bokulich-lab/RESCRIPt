2bcd267232548a369c2cb4b91ab6ab1f18278946
Nicholas Bokulich <nbokulich@gmail.com>
2020-07-03 06:15:39 -0700

    derep skbio (#58)
    This worked really well. The memory improvements showed. When I ran this on V4 ref data using the original implementation, the RAM usage spiked ~ 1 GB, and hovered ~ 880 MB. With the new implementation in this PR, the RAM usage spiked to half, ~ 500 MB, and hovered at ~ 330 MB. I suspect the RAM savings will grow with larger files.

--- rescript/cross_validate.py
+++ rescript/cross_validate.py
@@ -26,8 +26,8 @@ def evaluate_fit_classifier(ctx,
                             n_jobs=1,
                             confidence=0.7):
     '''
-    taxonomy: pd.Series of taxonomy labels
-    sequences: pd.Series of sequences
+    taxonomy: FeatureData[Taxonomy] artifact of taxonomy labels
+    sequences: FeatureData[Sequence] artifact of sequences
     k: number of kfold cv splits to perform.
     '''
     # Validate inputs
@@ -67,8 +67,8 @@ def evaluate_cross_validate(ctx,
                             n_jobs=1,
                             confidence=0.7):
     '''
-    taxonomy: pd.Series of taxonomy labels
-    sequences: pd.Series of sequences
+    taxonomy: FeatureData[Taxonomy] artifact of taxonomy labels
+    sequences: FeatureData[Sequence] artifact of sequences
     k: number of kfold cv splits to perform.
     random_state: random state for cv.
     '''
