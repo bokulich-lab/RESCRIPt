98063ed5a11c1776d3671f04fd5c25e193d19c0d
Michal Ziemski <mziemski@ethz.ch>
2020-10-15 17:39:11 +0200

    fixup! IMP: add alignment trimming action

--- rescript/trim_alignment.py
+++ rescript/trim_alignment.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020, QIIME 2 development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 from typing import Union
 
 import qiime2
