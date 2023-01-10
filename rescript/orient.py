1543a2543b3182b4f6a10e5f71d6d8deaa84ddb0
Fabio <miranda.fmm@gmail.com>
2023-01-10 19:47:27 +0100

    Replace usearch_global with orient

--- rescript/orient.py
+++ rescript/orient.py
@@ -19,7 +19,7 @@ def orient_seqs(sequences: DNAFASTAFormat,
                 threads: int = 1,
                 left_justify: bool = False,
                 ) -> (DNAFASTAFormat, DNAFASTAFormat):
-    matched_temp, notmatched = DNAFASTAFormat(), DNAFASTAFormat()
+    matched, notmatched = DNAFASTAFormat(), DNAFASTAFormat()
     if reference_sequences is not None:
         # use vsearch to search query seqs against reference database
         # report orientation of query seqs relative to reference seqs.
@@ -29,36 +29,22 @@ def orient_seqs(sequences: DNAFASTAFormat,
             # to upper but which is faster: disabling masking or looping
             # through with skbio?
             cmd = ['vsearch',
-                   '--usearch_global', str(sequences),
-                   '--matched', str(matched_temp),
+                   '--orient', str(sequences),
+                   '--fastaout', str(matched),
                    '--notmatched', str(notmatched),
                    '--db', str(reference_sequences),
-                   '--id', str(perc_identity),
-                   '--maxaccepts', '1',
-                   '--strand', 'both',
+                   # '--id', str(perc_identity),
+                   # '--maxaccepts', '1',
+                   # '--strand', 'both',
                    '--qmask', 'none',
-                   '--query_cov', str(query_cov),
+                   # '--query_cov', str(query_cov),
                    '--threads', str(threads),
-                   '--userfields', 'qstrand',
-                   '--userout', out.name]
-            if left_justify:
-                cmd.append('--leftjust')
+                   # '--userfields', 'qstrand',
+                   '--fastaout', out.name]
+            # if left_justify:
+            #     cmd.append('--leftjust')
             run_command(cmd)
-            with open(out.name, 'r') as orient:
-                orientations = [line.strip() for line in orient]
 
-        # if any query seqs are in reverse orientation, reverse complement
-        if '-' in orientations:
-            matched = DNAFASTAFormat()
-            with matched.open() as out_fasta:
-                for seq, orientation in zip(
-                        matched_temp.view(DNAIterator), orientations):
-                    if orientation == '+':
-                        seq.write(out_fasta)
-                    elif orientation == '-':
-                        seq.reverse_complement().write(out_fasta)
-        else:
-            matched = matched_temp
     else:
         matched = DNAFASTAFormat()
         with matched.open() as out_fasta:
