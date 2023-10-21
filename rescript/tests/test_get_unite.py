a3589c5c1bccbe418e77508d68d0eeb762926d65
Colin Brislawn <cbrisl@gmail.com>
2023-10-21 17:21:16 -0400

    Add alt tests for get_doi and get_url

--- rescript/tests/test_get_unite.py
+++ rescript/tests/test_get_unite.py
@@ -22,6 +22,9 @@ from urllib.request import urlopen
 from urllib.error import HTTPError
 from unittest.mock import patch
 
+# Global vars for use between functions
+obs_dois = []
+
 
 class TestGetUNITE(TestPluginBase):
     package = "rescript.tests"
@@ -74,12 +77,42 @@ class TestGetUNITE(TestPluginBase):
                     except HTTPError:
                         raise ValueError("Failed to open URL: " + url)
 
-    # depends on _unite_get_url
+    ##########
+    # Alternative versions of these two functions. Instead of using a
+    # hardcoded list of DOIs, they populate a global obs_dois list. 
+    ##########
+
+    # test by popbulating obs_dois list
+    def test_unite_get_doi2(self):
+        # All supported inputs to _unite_get_doi
+        versions = ["9.0", "8.3", "8.2"]
+        taxon_groups = ["fungi", "eukaryotes"]
+        singletons = [True, False]
+        # for all combinations
+        for v in versions:
+            for tg in taxon_groups:
+                for s in singletons:
+                    # add it to the list
+                    obs_dois.append(_unite_get_doi(v, tg, s))
+        if obs_dois:
+            self.assertTrue(True)
+
+    # Test by querying the newly full obs_dois list
+    def test_unite_get_url2(self):
+        for doi in obs_dois:
+            try:
+                url = _unite_get_url(doi)
+                urlopen(url)
+            except HTTPError:
+                raise ValueError("Failed to open URL: " + url)
+
+    # Download a single, small file from the API endpoint
     def test_unite_get_raw_files(self):
-        url = _unite_get_url(doi="10.15156/BIO/786385")
+        # Use a single (unrelated) download URL
+        url = "https://files.plutof.ut.ee/doi/C8/E4/C8E4A8E6A7C4C00EACE3499C51E550744A259A98F8FE25993B1C7B9E7D2170B2.zip" # noqa
         with tempfile.TemporaryDirectory() as tmpdirname:
-            # If file can download, pass this test
             _unite_get_tgz(url, tmpdirname)
+            # If function finished, pass this test
             self.assertTrue(True)
 
     def test_unite_get_artifacts(self):
