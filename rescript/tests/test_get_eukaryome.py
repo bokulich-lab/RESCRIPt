ab4ad2523ff78448092aa3ba23b6c94cd3692fd4
Mike <michael.robeson@colorado.edu>
2025-09-15 19:49:42 -0500

    added zip file handling text code

--- rescript/tests/test_get_eukaryome.py
+++ rescript/tests/test_get_eukaryome.py
@@ -11,7 +11,9 @@ from qiime2.plugins import rescript
 from rescript.get_eukaryome import (_assemble_rrna_url,
                                     _retrieve_data_from_eukaryome,
                                     _make_fasta_str, _make_taxonomy_df,
-                                    _process_eukaryome_data
+                                    _process_eukaryome_data,
+                                    _extract_zip_euk_seq_file,
+                                    _extract_7zip_euk_seq_file
                                     )
 from q2_types.feature_data import (TSVTaxonomyFormat,
                                    DNAFASTAFormat,
@@ -19,6 +21,10 @@ from q2_types.feature_data import (TSVTaxonomyFormat,
 from unittest.mock import patch
 import pandas as pd
 from pandas import DataFrame
+import os
+import tempfile
+import zipfile
+import py7zr
 
 
 class TestGetEukaryome(TestPluginBase):
@@ -26,11 +32,15 @@ class TestGetEukaryome(TestPluginBase):
 
     def setUp(self):
         super().setUp()
+        self.eukaryome_fasta = self.get_data_path('eukaryome-seqs.fasta')
         self.eukaryome_seqs = DNAFASTAFormat(
-                            self.get_data_path('eukaryome-seqs.fasta'),
+                            self.eukaryome_fasta,
+                            # self.get_data_path('eukaryome-seqs.fasta'),
                             mode='r')
+        self.eukaryome_parsed_fasta = \
+            self.get_data_path('eukaryome-parsed-seqs.fasta')
         self.eukaryome_parsed_seqs = DNAFASTAFormat(
-                            self.get_data_path('eukaryome-parsed-seqs.fasta'),
+                            self.eukaryome_parsed_fasta,
                             mode='r')
         self.eukaryome_parsed_tax = TSVTaxonomyFormat(
                             self.get_data_path('eukaryome-parsed-taxa.tsv'),
@@ -53,6 +63,43 @@ class TestGetEukaryome(TestPluginBase):
                  'https://sisu.ut.ee/wp-content/uploads/sites/643/'
                  'General_EUK_longread_v1.9.2.zip')
         self.assertEqual(obs_url, exp_url)
+
+    def test_extract_zip_euk_seq_file(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            zfn = os.path.join(tmpdirname, 'eukaryome-pseqs.zip')
+            with zipfile.ZipFile(zfn, "w", zipfile.ZIP_DEFLATED) as myzip:
+                myzip.write(self.eukaryome_parsed_fasta,
+                            arcname='eukaryome-pseqs.fasta')
+
+            obs_uzs = _extract_zip_euk_seq_file(
+                        tmpdirname, 'eukaryome-pseqs', zfn)
+            exp_seqs_dict = {seq.metadata['id'].split(';', 1)[0]: str(seq)
+                             for seq in self.eukaryome_parsed_seqs.view(
+                                DNAIterator)}
+            obs_seqs_dict = {seq.metadata['id']: str(seq)
+                             for seq in obs_uzs}
+            self.assertEqual(exp_seqs_dict, obs_seqs_dict)
+
+    def test_extract_7zip_euk_seq_file(self):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            zfn = os.path.join(tmpdirname, 'eukaryome-pseqs.zip')
+            with zipfile.ZipFile(zfn, "w", zipfile.ZIP_DEFLATED) as myzip:
+                # myzip.write(self.eukaryome_parsed_fasta,
+                #             arcname='eukaryome-pseqs.fasta')
+                szfn = os.path.join(tmpdirname, 'eukaryome-pseqs.7z')
+                with py7zr.SevenZipFile(szfn, 'w') as my7z:
+                    my7z.write(self.eukaryome_parsed_fasta,
+                               arcname='eukaryome-pseqs.fasta')
+                myzip.write(szfn, arcname='eukaryome-pseqs.7z')
+
+            obs_uzs = _extract_7zip_euk_seq_file(
+                        tmpdirname, 'eukaryome-pseqs', zfn)
+            exp_seqs_dict = {seq.metadata['id'].split(';', 1)[0]: str(seq)
+                             for seq in self.eukaryome_parsed_seqs.view(
+                                 DNAIterator)}
+            obs_seqs_dict = {seq.metadata['id']: str(seq)
+                             for seq in obs_uzs}
+            self.assertEqual(exp_seqs_dict, obs_seqs_dict)
 
     def test_make_fasta_str(self):
         seqid = 'Seq01'
