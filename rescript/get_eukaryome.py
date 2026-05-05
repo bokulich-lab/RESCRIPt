55fbcc9b0a38a2b64cf47e816aea9ae583f08212
Mike <michael.robeson@colorado.edu>
2026-05-04 19:16:21 -0500

    added eukaryome db v2, and refactored code & tests.

--- rescript/get_eukaryome.py
+++ rescript/get_eukaryome.py
@@ -19,7 +19,7 @@ RRNA_GENE_LIST = ['SSU', 'LSU', 'ITS', 'longread', 'all']
 
 
 def _assemble_rrna_url(rrna_gene,
-                       version='1.9.4',
+                       version='2.0',
                        ):
 
     base_url = ('https://sisu.ut.ee/wp-content/uploads/sites/643/'
@@ -46,26 +46,31 @@ def _get_eukaryome_data_path(tmpdirname, url, basename):
 
 
 def _extract_zip_euk_seq_file(tmpdirname, uncompressed_base_fn, fasta_zfp):
-    uncompressed_fn = uncompressed_base_fn + '.fasta'
-    out_path = os.path.join(tmpdirname, uncompressed_fn)
-    with zipfile.ZipFile(fasta_zfp, 'r') as zipf:
-        zipf.extract(uncompressed_fn, tmpdirname)
-        seqs = DNAFASTAFormat(out_path, mode="r").view(DNAIterator)
-    return seqs
-
-
-def _extract_7zip_euk_seq_file(tmpdirname, uncompressed_base_fn, fasta_zfp):
+    # In later DBs, ITS files may not be within a nested 7z file.
     uncompressed_7z_fn = uncompressed_base_fn + '.7z'
     uncompressed_fn = uncompressed_base_fn + '.fasta'
     compressed_out_path = os.path.join(tmpdirname,  uncompressed_7z_fn)
     uncompressed_out_path = os.path.join(tmpdirname, uncompressed_fn)
 
     with zipfile.ZipFile(fasta_zfp, 'r') as zipf:
-        zipf.extract(uncompressed_7z_fn, tmpdirname)
-        with py7zr.SevenZipFile(compressed_out_path, mode='r') as zip7f:
-            zip7f.extract(path=tmpdirname, targets=[uncompressed_fn])
+        nl = zipf.namelist()
+        if uncompressed_fn in nl:
+            zipf.extract(uncompressed_fn, tmpdirname)
             seqs = DNAFASTAFormat(uncompressed_out_path,
                                   mode="r").view(DNAIterator)
+        elif uncompressed_7z_fn in nl:
+            zipf.extract(uncompressed_7z_fn, tmpdirname)
+            with py7zr.SevenZipFile(compressed_out_path, mode='r') as zip7f:
+                zip7f.extract(path=tmpdirname, targets=[uncompressed_fn])
+                seqs = DNAFASTAFormat(uncompressed_out_path,
+                                      mode="r").view(DNAIterator)
+        else:
+            raise FileNotFoundError(
+                            "Neither the %s nor the "
+                            "%s files were found within "
+                            "the archive." % (uncompressed_7z_fn,
+                                              uncompressed_fn)
+                            )
     return seqs
 
 
@@ -112,14 +117,10 @@ def _retrieve_data_from_eukaryome(rrna_url, gene):
                                              basename)
 
         print('  Processing \'{0}\' data ...'.format(gene))
-        if gene == 'ITS':
-            seqs = _extract_7zip_euk_seq_file(tmpdirname,
-                                              uncompressed_base_fn,
-                                              fasta_zfp)
-        else:
-            seqs = _extract_zip_euk_seq_file(tmpdirname,
-                                             uncompressed_base_fn,
-                                             fasta_zfp)
+
+        seqs = _extract_zip_euk_seq_file(tmpdirname,
+                                         uncompressed_base_fn,
+                                         fasta_zfp)
 
         print('    Processing sequences ...')
         proc_seqs, proc_tax = _process_eukaryome_data(seqs)
@@ -128,7 +129,7 @@ def _retrieve_data_from_eukaryome(rrna_url, gene):
 
 def get_eukaryome_data(
     rrna_gene: list,
-    version: str = '1.9.4',
+    version: str = '2.0',
         ) -> (DNAFASTAFormat, TSVTaxonomyFormat):
 
     seq_res = {}
